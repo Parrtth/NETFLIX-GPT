eae908899524df58bf002c7f511c4081d1b018b5
parth <patelparth7719@gmail.com>
2025-07-03 19:40:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <p className='text-3xl font-bold underline text-6xl text-red-600'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
