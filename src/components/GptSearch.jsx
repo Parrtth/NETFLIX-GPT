d5671b649a1fb96af1fc9c3da3a6b3bd8e52a213
parth <patelparth7719@gmail.com>
2025-12-08 23:42:05 +0530

    chore: fine tune gptsearch page

--- src/components/GptSearch.jsx
+++ src/components/GptSearch.jsx
@@ -5,23 +5,16 @@ import { BG_URL } from '../utils/constants'
 
 const GptSearch = () => {
   return (
-    <div className="relative min-h-screen">
-      <div
-        className="fixed top-0 left-0 w-full h-screen -z-10"
-        style={{
-          backgroundImage: `url(${BG_URL})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+    <>
+      <div className="fixed inset-0 -z-10">
+        <img className="h-full w-full object-cover" src={BG_URL} alt="background" />
+        <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/40 to-black/80"></div>
       </div>
-
       <div className="pt-32 md:pt-[10%] mx-auto w-full max-w-6xl px-4 md:px-6">
         <GptSearchBar />
         <GptMovieSuggestions />
       </div>
-    </div>
+    </>
   )
 }
 
